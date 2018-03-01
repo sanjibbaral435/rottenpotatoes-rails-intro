d66451820706c5c156a79135febe064a24b8179f
EC2 Default User <ec2-user@ip-172-31-10-197.us-east-2.compute.internal>
2018-03-01 23:52:51 +0000

    part 3 bug fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,2 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        a = Array.new
-        self.select("rating").uniq.each {|el| a.push(el.rating)}
-        a.sort.uniq
-    end
 end
